1c4f001ca29b92a7c6eb9ec9d70be7331357ea7d
SonJava10Rikkei <sonjapan7@gmail.com>
2023-01-02 18:48:44 +0700

    a

--- Node_ExpressJS/2_Kien_thuc_cot_loi/10_Template_engine(handlebars)/main.js
+++ Node_ExpressJS/2_Kien_thuc_cot_loi/10_Template_engine(handlebars)/main.js
@@ -1,4 +1,38 @@
 /*
+Template engines (handlebars)
+
+truy cập trang web
+
+https://www.npmjs.com/package/express-handlebars
+
+Cài bằng dòng lệnh ở bảng Terminal
+
+$ npm install express-handlebars --save
+
+------ Tạo 1 thư mục src ở ngoài ------
+
+B1. Dán code vào file index.js
+
+const exphbs = require('express-handlebars');
 
 
+// Template engines(handlebars)
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+
+Đổi 
+app.get("/", (req, res) => {
+    var a = 1;
+    var b = 2;
+
+    var c = a + b;
+    res.send("123");
+});
+
+thành 
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
 */
